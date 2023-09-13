a9c9eb4cc1d1055fe77907e077c6ac3708bf91ca
wierza <dawid@wierzycki.pl>
2023-09-13 08:08:59 +0200

    Update color variables

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './FeatureBox.module.scss';
 
-const FeatureBox = ({ active, icon, children }) => (
-  <div className={styles.root + (active ? ' ' + styles.active : '')}>
+const FeatureBox = ({ icon, children }) => (
+  <div className={styles.root}>
     {icon && (
       <div className={styles.iconWrapper}>
         <FontAwesomeIcon className={styles.icon} icon={icon} />
       </div>
     )}
-    <div className={styles.content}>{children}</div>
+    <Link to={'#'} className={styles.content}>
+      {children}
+    </Link>
   </div>
 );
 
