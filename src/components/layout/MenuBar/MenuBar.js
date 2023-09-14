f7c5bd917f3680f2fa635f07a64cdf83ad38c9e7
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-09-14 14:42:26 +0200

    WDP230901 start working on RWD for main menu

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -1,48 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import ProductSearch from '../../features/ProductSearch/ProductSearch';
 
 import styles from './MenuBar.module.scss';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const MenuBar = ({ children }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col'>
-          <ProductSearch />
-        </div>
-        <div className={'col-auto ' + styles.menu}>
-          <ul>
-            <li>
-              <a href='#' className={styles.active}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>Furniture</a>
-            </li>
-            <li>
-              <a href='#'>Chair</a>
-            </li>
-            <li>
-              <a href='#'>Table</a>
-            </li>
-            <li>
-              <a href='#'>Sofa</a>
-            </li>
-            <li>
-              <a href='#'>Bedroom</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
+const MenuBar = ({ children }) => {
+  const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='align-items-center d-flex flex-row flex-md-column-reverse flex-xl-row'>
+          <div className='d-block d-md-none'>
+            <FontAwesomeIcon
+              icon={isHamburgerClicked ? faTimes : faBars}
+              onClick={() => setIsHamburgerClicked(!isHamburgerClicked)}
+            ></FontAwesomeIcon>
+          </div>
+
+          <div className='col'>
+            <ProductSearch />
+          </div>
+          <div
+            className={`col-auto d-none d-md-block ${styles.menu} {${isHamburgerClicked} ? ${styles.open}:"}`}
+          >
+            <ul>
+              <li>
+                <a href='#' className={styles.active}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>Furniture</a>
+              </li>
+              <li>
+                <a href='#'>Chair</a>
+              </li>
+              <li>
+                <a href='#'>Table</a>
+              </li>
+              <li>
+                <a href='#'>Sofa</a>
+              </li>
+              <li>
+                <a href='#'>Bedroom</a>
+              </li>
+              <li>
+                <a href='#'>Blog</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 MenuBar.propTypes = {
   children: PropTypes.node,
