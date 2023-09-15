326e8df494ba273a48a5b6d69625d1b252c8aed7
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-09-15 18:27:48 +0200

    WDP230901-25 generate random product for promotion, change test component

--- src/components/features/Sale/Sale.js
+++ src/components/features/Sale/Sale.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAll } from '../../../redux/productsRedux';
 
 import styles from './Sale.module.scss';
 
-const Sale = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div>Test</div>
-      <div>
-        <div>Test2</div>
-        <div>Test 3</div>
+const Sale = () => {
+  const products = useSelector(state => getAll(state));
+  let randomProductIndex = Math.floor(Math.random() * products.length + 1);
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div>Test</div>
+        <img
+          src={products[randomProductIndex].image}
+          alt={products[randomProductIndex].name}
+        />
+
+        <div>
+          <div>Test2</div>
+          <div>Test 3</div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Sale;
--- src/components/features/Sale/Sale.test.js
+++ src/components/features/Sale/Sale.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Sale from './Sale';
+import { Provider } from 'react-redux';
+import store from '../../../redux/store';
 
 describe('Component Sale', () => {
   it('should render without crashing', () => {
-    const component = shallow(<Sale />);
+    const component = shallow(
+      <Provider store={store}>
+        <Sale />
+      </Provider>
+    );
     expect(component).toBeTruthy();
   });
 });
