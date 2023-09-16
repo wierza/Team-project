dde87749a2090a55ce4cb9c4ca64eeecaf211638
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-09-16 18:31:21 +0200

    WDP230901-25 add imgs, start working on styles

--- src/components/features/Sale/Sale.js
+++ src/components/features/Sale/Sale.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getAll } from '../../../redux/productsRedux';
+import { getProductByCategory } from '../../../redux/productsRedux';
 
 import styles from './Sale.module.scss';
 
 const Sale = () => {
-  const products = useSelector(state => getAll(state));
-  let randomProductIndex = Math.floor(Math.random() * products.length + 1);
+  const sofas = useSelector(state => getProductByCategory(state, 'sofa'));
+  let randomSofaIndex = Math.floor(Math.random() * (sofas.length + 1));
 
   return (
     <div className={styles.root}>
       <div className={`container ${styles.mainContainer}`}>
         <div className={styles.leftSection}>
-          {/* <img
-            src={products[randomProductIndex].image}
-            alt={products[randomProductIndex].name}
-          /> */}
+          <img src={sofas[randomSofaIndex].image} alt={sofas[randomSofaIndex].name} />
+          <p className={styles.category}>{sofas[randomSofaIndex].category}</p>
+          <p className={styles.name}>{sofas[randomSofaIndex].name}</p>
+          <div className={styles.discount}>-20%</div>
         </div>
         <div className={styles.rightSection}>
           <div className={styles.rightUpPicture}>
-            {/* <img
-              src={products[randomProductIndex].image}
-              alt={products[randomProductIndex].name}
-            /> */}
+            <img
+              src={'./images/promo/officeChair1.jpg'}
+              alt={sofas[randomSofaIndex].name}
+            />
           </div>
           <div className={styles.rightDownPicture}>
-            {/* <img
-              src={products[randomProductIndex].image}
-              alt={products[randomProductIndex].name}
-            /> */}
+            <img
+              src={'./images/promo/bedPromo.jpg'}
+              alt={sofas[randomSofaIndex].name}
+            />
+            <p className={styles.collection}>
+              <b>Special</b> Collection
+            </p>
+            <p className={styles.save}>Save up 45% of furniture </p>
           </div>
         </div>
       </div>
