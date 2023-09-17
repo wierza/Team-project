661cf562f303ef68d3a4ba7b99c4ecbf158fd9ae
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-09-17 17:37:04 +0200

    Add 'Brands' to Homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,18 +1,16 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-
 import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import BrandsSection from '../../features/BrandsSection/BrandsSection';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
     <NewFurniture />
+    <BrandsSection />
   </div>
 );
-
-// Homepage.propTypes = {};
 
 export default Homepage;
