9e7a7c0817d4fc98b5b6f60c051526c7693c6bca
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-09-20 19:31:03 +0200

    add actions for arrows and swipable

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { getAll } from '../../../redux/commentsRedux.js';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import styles from './Feedback.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faQuoteRight,
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 import Coment from '../../common/Coment/Coment.js';
+import Swipeable from '../../common/Swipeable/Swipeable.js';
+import Button from '../../common/Button/Button';
 import { useTranslation } from 'react-i18next';
 
 const Feedback = () => {
@@ -12,46 +19,95 @@ const Feedback = () => {
 
   const comments = useSelector(getAll);
   const commentCount = comments.length;
+  const [activeComment, setActiveComment] = useState(1);
+  const [fade, setFade] = useState(true);
+
+  const comentLeftAction = e => {
+    e.preventDefault();
+    if (activeComment > 0) {
+      setFade(false);
+      setTimeout(() => {
+        setActiveComment(activeComment - 1);
+        setFade(true);
+      }, 500);
+    }
+  };
+
+  const comentRightAction = e => {
+    e.preventDefault();
+    if (activeComment < commentCount - 1) {
+      setFade(false);
+      setTimeout(() => {
+        setActiveComment(activeComment + 1);
+        setFade(true);
+      }, 500);
+    }
+  };
+  const handleCommentChange = i => {
+    setFade(false);
+    setTimeout(() => {
+      setActiveComment(i);
+      setFade(true);
+    }, 500);
+  };
 
   const dots = [];
   for (let i = 0; i < commentCount; i++) {
     dots.push(
-      <li>
-        <a
-          onClick={() => this.handlePageChange(i)}
-          className={i === 0 && styles.active}
+      <li key={i}>
+        <p
+          onClick={() => handleCommentChange(i)}
+          className={i === activeComment && styles.active}
         >
-          page {i}
-        </a>
+          comment {i}
+        </p>
       </li>
     );
   }
 
   return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={styles.panelBar}>
-          <div className='row no-gutters align-items-end'>
-            <div className={'col-auto ' + styles.heading}>
-              <h3>{t('feedback.feedback')}</h3>
-            </div>
-            <div className={'col-auto ' + styles.dots}>
-              <ul>{dots}</ul>
+    <Swipeable onLeftSwipe={comentLeftAction} onRightSwipe={comentRightAction}>
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={styles.panelBar}>
+            <div className='row no-gutters align-items-end'>
+              <div className={'col-auto ' + styles.heading}>
+                <h3>{t('feedback.feedback')}</h3>
+              </div>
+              <div className={'col-auto ' + styles.dots}>
+                <ul>{dots}</ul>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='row justify-content-center'>
-          <div className='col-10'>
-            <div className={styles.paragraph}>
-              <FontAwesomeIcon icon={faQuoteRight}></FontAwesomeIcon>
-            </div>
-            <div>
-              <Coment {...comments[0]} />
+          <div className={styles.swipeButton}>
+            <Button
+              variant='outline'
+              onClick={comentLeftAction}
+              className={styles.leftButton}
+            >
+              <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+            </Button>
+            <Button
+              variant='outline'
+              onClick={comentRightAction}
+              className={styles.rightButton}
+            >
+              <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+            </Button>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='col-10'>
+              <div className={styles.paragraph}>
+                <FontAwesomeIcon icon={faQuoteRight}></FontAwesomeIcon>
+              </div>
+              <div className={`row + ${fade ? styles.fadeIn : styles.fadeOut}`}>
+                <Coment {...comments[activeComment]} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Swipeable>
   );
 };
 
